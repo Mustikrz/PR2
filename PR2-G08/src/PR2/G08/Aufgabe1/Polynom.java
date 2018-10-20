5d6670a344ff5b38a41f8f2cd3dd4c0f376a6a29
Mustafa <Mustafa@192.168.0.150>
2018-10-20 19:54:06 +0200

    Methode berechne und aufleiten wurde gemacht, aber nochnicht auf richtigkeit überprüft.

--- PR2-G08/src/PR2/G08/Aufgabe1/Polynom.java
+++ PR2-G08/src/PR2/G08/Aufgabe1/Polynom.java
@@ -21,14 +21,37 @@ public class Polynom {
     
     /* ******** Methoden ******** */
     
+  //Berechnen des Wertes des Polynoms fur ein beliebiges x
+    public double berechne(double x) {
+    	//Initialisierung vom Funktionswert
+    	double funktionswert = 0;
+    	
+    	for(int i=getGrad(); i>=0; i--){
+    		//Funktionswert = Koeffizient * x^Grad
+    		funktionswert = funktionswert + (Math.pow(x,i)*koeffizient[i]);
+    	
+    	}
+    	return funktionswert;
+    }
     
     
     
+    //Berechnen des Integrals des Polynoms
+    public double[] integriere (){
+    	//Für das Integral muss man das Polynom aufleiten um eine Stammfunktion zu bilden.
+    	//Beim aufleiten eines Polynoms bekommt man am Ende eine Variable die unendlich viele Zahlen
+    	//haben kann, daher muss das Array um eins vergrößert werden.
+    	double[] stammfunktion = new double[koeffizient.length + 1];
+    	
+    	for(int i=0;i<=koeffizient.length;i++){
+    		stammfunktion[i+1] = koeffizient[i]/ (i+1); 	//Aufleiten von x^2
+    														//x^2 -> (x^3)/3
+    	}
+    	return stammfunktion;
+    }
     
     
 
-    
-    
     /* ******** Getter - Methoden ******** */
     
     //Auslesen aller Koeffizienten in einem Schritt
